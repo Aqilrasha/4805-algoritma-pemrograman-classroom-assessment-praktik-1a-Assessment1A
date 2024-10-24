8a7ddd3d8b59d0687ea184205db38b2991b19b49
Aqilrasha <aqilarasha2@gmail.com>
2024-10-24 10:12:12 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -1,5 +1,28 @@
 public class Nomor1{
     public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Masukkan total jam lembur: ");
+        int jamLembur = scanner.nextInt();
+        double honorLembur;
+
+        if (jamLembur <= 16) {
+            honorLembur = jamLembur * 10000;
+        } else {
+            // Honor lembur untuk 16 jam pertama
+            double honor16Jam = 16 * 10000;
+            // Hitung sisa jam lembur
+            int sisaJam = jamLembur - 16;
+            // Honor lembur untuk sisa jam
+            double honorSisaJam = sisaJam * 15000;
+            // Total honor lembur
+            honorLembur = honor16Jam + honorSisaJam;
+        }
+
+        System.out.println("Total jam lembur: " + jamLembur);
+        System.out.println("Honor lembur: " + honorLembur);
+
+        scanner.close();
     }
 }
+        
