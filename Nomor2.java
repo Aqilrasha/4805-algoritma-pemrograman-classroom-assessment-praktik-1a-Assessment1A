2b433a74345dc78bbd7113f0fae2a03040696310
Aqilrasha <aqilarasha2@gmail.com>
2024-10-24 10:29:27 +0700

    Update Nomor2.java

--- Nomor2.java
+++ Nomor2.java
@@ -1,5 +1,25 @@
 public class Nomor2{
     public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int omzet = 13000 
+        String nama = "Beliau/Mereka/Kita";
 
+        //Lama Trip Beliau
+        int penarikan = 13000;
+        String drive = "Beliau";
+        System.out.println(4 * 13000);
+
+        //Lama Trip Kita
+        int kita = 13000;
+        String bapakDrive = "kita";
+        System.out.println(5 * 13000);
+
+        //Lama Trip Beliau
+        int beliau = 13000;
+        String beliauDrive = "Beliau";
+        System.out.println(2 * 13000);
+
+        int totalPenarikan = 52000 + 65000 + 26000;
+        System.out.println("beliau" + "kita" + "beliau");
     }
 }
